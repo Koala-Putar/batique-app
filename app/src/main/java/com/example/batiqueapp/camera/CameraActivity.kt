e1453e70d3decb679f2c2b6250bf2d89a4e2c159
WillyFerry <willyferry001@gmail.com>
2021-05-31 00:04:24 +0700

    implmentation tflite model to camera activity

--- app/src/main/java/com/example/batiqueapp/camera/CameraActivity.kt
+++ app/src/main/java/com/example/batiqueapp/camera/CameraActivity.kt
@@ -1,22 +1,135 @@
 package com.example.batiqueapp.camera
 
-import android.content.Intent
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.batiqueapp.MainActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import com.example.batiqueapp.databinding.ActivityCameraBinding
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.common.model.LocalModel
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.objects.ObjectDetection
+import com.google.mlkit.vision.objects.ObjectDetector
+import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions
 
 class CameraActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCameraBinding
+
+    private lateinit var objectDetector: ObjectDetector
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    companion object {
+        private val PERMISSIONS_REQUEST = 1
+
+        private val PERMISSION_CAMERA = Manifest.permission.CAMERA
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.backButton.setOnClickListener {
-            startActivity(Intent(this@CameraActivity, MainActivity::class.java))
+        if (!hasPermission()) {
+            requestPermission()
+        }
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider = cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+
+        val localModel = LocalModel.Builder()
+                .setAbsoluteFilePath("object_detection.tflite")
+                .build()
+
+        val customObjectDetectorOptions = CustomObjectDetectorOptions.Builder(localModel)
+                .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
+                .enableClassification()
+                .setClassificationConfidenceThreshold(0.5f)
+                .setMaxPerObjectLabelCount(3)
+                .build()
+
+        objectDetector = ObjectDetection.getClient(customObjectDetectorOptions)
+    }
+
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+
+        val preview = Preview.Builder().build()
+
+        val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+
+        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+
+        val imageAnalysis = ImageAnalysis.Builder()
+                .setTargetResolution(Size(224, 224))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), { imageProxy ->
+
+            val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+            val image = imageProxy.image
+
+            if (image != null) {
+                val processImage = InputImage.fromMediaImage(image, rotationDegrees)
+                objectDetector
+                        .process(processImage)
+                        .addOnSuccessListener { objects ->
+                            for (i in objects) {
+                                if(binding.parentPreviewView.childCount > 1) binding.parentPreviewView.removeViewAt(1)
+                                val element = Draw(context = this,
+                                        rect = i.boundingBox,
+                                        text = i.labels.firstOrNull()?.text ?: "Undefined")
+
+                                binding.parentPreviewView.addView(element)
+                            }
+                            imageProxy.close()
+                        }
+                        .addOnFailureListener {
+                            Log.v("CameraActivity", "Error - ${it.message}")
+                            imageProxy.close()
+                        }
+            }
+        })
+        cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, imageAnalysis, preview)
+    }
+
+    private fun hasPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    }
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA)) {
+                Toast.makeText(
+                        this@CameraActivity,
+                        "Camera permission is required for this demo",
+                        Toast.LENGTH_LONG)
+                        .show()
+            }
+            requestPermissions(arrayOf(PERMISSION_CAMERA), PERMISSIONS_REQUEST)
         }
     }
 }
